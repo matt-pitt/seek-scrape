eae445dbdbf42ebd7a876eb68932ce3229bc5e7b
Matthew Pitt <Matthew.Pitt@mk.com.au>
2021-03-27 14:50:15 +1100

    completed seek.com.au scrape

--- scripts/seek_scrape.R
+++ scripts/seek_scrape.R
@@ -2,26 +2,40 @@ library(here)
 library(rvest)
 library(tidyverse)
 
+total_da_jobs <- "https://www.seek.com.au/jobs/in-All-Melbourne-VIC/full-time?keywords=%27Data%20Analyst%22&page=5&salaryrange=0-999999&salarytype=annual#start-of-content"
+tot_job <- read_html(total_da_jobs) %>% html_nodes("#SearchSummary ._7ZnNccT") %>% html_text()
+
 da_seek_html <- "https://www.seek.com.au/jobs/in-All-Melbourne-VIC/full-time?keywords=%27Data%20Analyst%22&page=5&salaryrange=100000-999999&salarytype=annual#start-of-content"
+tot_job_over_100k <- read_html(da_seek_html) %>% html_nodes("#SearchSummary ._7ZnNccT") %>% html_text()
 
-da_seek_html_two <- "https://www.seek.com.au/jobs/in-All-Melbourne-VIC/full-time?keywords=%27Data%20Analyst%22&page=2&salaryrange=100000-999999&salarytype=annual"
+# total jobs now stored
+
+# seek pages to extract
+pages <- 1:10
+
+# initialise df
+
+df <- data.frame(title = character(0),
+                 company = character(0),
+                 industry = character(0),
+                 job_desc = character(0))
 
 
-
-
-
-seek <- read_html(da_seek_html)
-seek_two <- read_html(da_seek_html_two)
-
-seek_title <- seek %>% html_nodes("._2S5REPk") %>% html_text(trim = TRUE)
-job_desc <- seek %>% html_nodes("._2OKR1ql") %>% html_text(trim = TRUE)
-company <- seek %>% html_nodes("._2Ryjovs+ ._2Ryjovs ._17sHMz8") %>% html_text(trim = TRUE)
-industry <- seek %>% html_nodes("._1edaxlv ._7ZnNccT ._17sHMz8") %>% html_text(trim = TRUE)
+for(i in pages){
   
+  site <- paste0("https://www.seek.com.au/jobs/in-All-Melbourne-VIC/full-time?keywords=%27Data%20Analyst%22&page=",i,"&salaryrange=100000-999999&salarytype=annual#start-of-content")
 
-df <- bind_cols(job_title = seek_title,
-                company = company,
-                industry = industry,
-                job_desc = job_desc)
+  stats <- data.frame(title = read_html(site) %>% html_nodes("._2S5REPk") %>% html_text(trim = TRUE),
+                      company = read_html(site) %>% html_nodes("._2Ryjovs+ ._2Ryjovs :nth-child(2)") %>% html_text(trim = TRUE),
+                      industry = read_html(site) %>% html_nodes("._1edaxlv ._7ZnNccT ._17sHMz8") %>% html_text(trim = TRUE),
+                      job_desc = read_html(site) %>% html_nodes("._2OKR1ql") %>% html_text(trim = TRUE))
 
-df
+  stats$title <- stats$title
+  stats$company <- stats$company
+  stats$industry <- stats$industry
+  stats$job_desc <- stats$job_desc
+  
+  df <- rbind(df, stats)
+  
+}
+
